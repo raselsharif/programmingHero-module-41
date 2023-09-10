0cca5f1bffe3ab47f458071db7cf9da613d15213
Rasel Sharif <raselsharif9@gmail.com>
2023-09-10 22:34:50 +0600

    added local json fake data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Watch from './assets/components/Watch/Watch'
+
+function App() {
+
+  return (
+    <>
+      <h1>ES6 Modules Overview</h1>
+       <Watch></Watch>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Watch/Watch.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+function Watch() {
+
+    const [watches, setWacthes] = useState([])
+    // console.log(watches);
+
+    useEffect(() => {
+        fetch('Watch.json')
+            .then(res => res.json())
+            .then(data => setWacthes(data))
+    }, [])
+
+    return (
+        <>
+            <div>
+                <h2>Watches: {watches.length}</h2>
+            </div>
+            {
+                watches.map(watch => <WatchSingle key={watch.watch_id} watch={watch}></WatchSingle>)
+                
+            }
+        </>
+    );
+}
+
+const WatchSingle = (watch) => {
+    console.log(watch);
+    const { brand,image_url
+    } = watch.watch
+    return (
+        <div>
+            <h3>Watch Brand: {brand}</h3>
+            <img src={image_url} alt="image not found"></img>
+        </div>
+    )
+}
+
+export default Watch;
